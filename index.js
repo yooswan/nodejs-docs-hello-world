74f2f14ce09d3843a96abcd2c637a2ab81f8afcc
Ubuntu <yooswan@gmail.com>
2018-08-24 06:31:04 +0000

    File updates for CD pipeline

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hel World!");
+    response.end("This is my first app on Azure!");
 
 });
 
